cc10930fcb571af6f16d10f489d2331968be8fd3
shivansh172004 <shivanshsharma98309@gmail.com>
2025-08-10 16:50:08 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,7 +55,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10) // yeh jo 10 hai means kitne rounds du?
+    this.password = await bcrypt.hash(this.password, 10) // yeh jo 10 hai means kitne rounds du?
     next()
 })
 
