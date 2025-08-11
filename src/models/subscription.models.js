6de4cbda9fbd5eca04283a06ad2a133aaed6b82c
shivansh172004 <shivanshsharma98309@gmail.com>
2025-08-11 23:47:22 +0530

    Add suer account management endpoints

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -9,7 +9,7 @@ const subscriptionSchema = new Schema({
     type: Schema.Types.ObjectId, // one to whom subscriber is subscribing
     ref: "User"
   }
-},{timestamps: true}
+},{timestamps:true}
 )
 
 
